9569c517f81e11fd812ce43e4819a7d7f847545d
Gra-Vi-Ty <worakan223@gmail.com>
2023-08-03 17:46:11 +0700

    uppp

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -11,3 +11,13 @@
     Output:
         Result: D C B A
 */
+
+#include <stdio.h>
+
+int main() {
+    char a ,b ,c, d;
+    scanf( "%c %c %c %c", &a, &b, &c, &d) ;
+    printf( "Result: %c %c %c %c", d, c, b, a) ;
+
+    return 0 ;
+}
